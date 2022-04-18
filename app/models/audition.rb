09300abc21e0ca17e8edf2fbbf2d44cf16ecef0d
Zachary Sain <zachsain@gmail.com>
2022-04-18 12:14:18 -0500

    commit tree 03cd45ceb0d20b32559902704b6f876f59c1ca61

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,10 @@
 class Audition < ActiveRecord::Base
-  
+  has_many :roles
+
+
+  def tester 
+    self.roles.map do |r|
+        puts r.character_name
+    end 
+  end 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# Audition.create(actor: "PJ", location: "Who knows", phone: "910300330", hired: false)
+# Audition.create(actor: "RR", location: "Tennesse", phone: "83283281", hired: false)
+# Audition.create(actor: "BDK", location: "Texas", phone: "3484384", hired: false)
+# Audition.create(actor: "Rdj", location: "Missi", phone: "382893218", hired: false)
+# Audition.create(actor: "JAJD", location: "JHDHDHD", phone: "83743782", hired: false)
+# Audition.create(actor: "DHHDS", location: "Joshn", phone: "832832183", hired: false)
+
+
+# Role.create(character_name: "Charlie")
+# Role.create(character_name: "Jamie")
+# Role.create(character_name: "Houston")
+# Role.create(character_name: "KLandh")
+# Role.create(character_name: "Billy")
+# Role.create(character_name: "Wondoa")
